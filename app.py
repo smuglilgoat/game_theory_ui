47217be20457e1413e6d17b32f59a76600f2c92c
smuglilgoat <47540165+smuglilgoat@users.noreply.github.com>
2020-08-31 21:52:33 +0100

    fully functional csv generator

--- app.py
+++ app.py
@@ -1,46 +1,110 @@
-"""
-p1 = [[-6, 0], [-10, -1]]
-p2 = [[-6, 0], [-10, -1]]
-"""
-p1 = [[0, 2], [10, -1]]
-p2 = [[0, 11], [2, 0]]
-"""
-p1 = [[0, 6, 4], [6, 0, 4], [3, 3, 5]]
-p2 = [[6, 0, 3], [0, 6, 3], [3, 3, 5]]
-"""
-def nashEquilibrium(p1, p2):
-    betterPayoff = False
-    equil = []
-    equiList = []
-    print("# Nash Equilibrium :")
-    for strP1 in range(len(p1)):
-        for strP2 in range(len(p2)):
-            betterPayoff = False
-            equil = []
-            if p1[strP1][strP2] < max(p1[strP1]):
-                betterPayoff = True
-            for str in p1:
-                if p1[strP1][strP2] < str[strP2]:
-                    betterPayoff = True
-            if p2[strP2][strP1] < max(p2[strP2]):
-                betterPayoff = True
-            for str in p2:
-                if p2[strP2][strP1] < str[strP1]:
-                    betterPayoff = True
-            if not betterPayoff:
-                print("Found Equilibrium : (", p1[strP1][strP2], ",", p2[strP2][strP1], ")")
-                equil.append(p1[strP1][strP2])
-                equil.append(p2[strP2][strP1])
-                equiList.append(equil)
-            else:
-                print("Not Equilibrium : (", p1[strP1][strP2], ",", p2[strP2][strP1], ")")
-    print("Nash Equilibriums :", equiList)
-def main():
-    print("# Players :")
-    global p1
-    print("Player 1: ", p1)
-    global p2
-    print("Player 2: ", p2)
-    nashEquilibrium(p1, p2)
+import sys
+import numpy as np
+import pandas as pd
+import itertools
 
-main()
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton
+from PyQt5.uic import loadUi
+
+numPlayers = 2
+playersStrats = np.array([2, 2])
+stratInputs = []
+
+class Window(QMainWindow):
+    """Main window."""
+    def __init__(self, parent=None):
+        """Initializer."""
+        super().__init__(parent)
+        # Load the main GUI
+        loadUi("main.ui", self)
+        # Connecting buttons with there function
+        self.actionNGPure.triggered.connect(self.onNGPureActnTrggrd)
+
+    # Create a slot for launching the player strategies dialog
+    def onNGPureActnTrggrd(self):
+        """Launch the player strat dialog."""
+        dlg = PlayerStarts(self)
+        dlg.exec()
+
+
+class PlayerStarts(QDialog):
+    """Employee dialog."""
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        # Load the dialog's GUI
+        loadUi("inputForm.ui", self)
+        # Connecting buttons with there function
+        self.addPlayerButton.clicked.connect(self.onAddPlayerBtnClicked)
+        self.generateButton.clicked.connect(self.onGenerateCSVBtnClicked)
+        # Inits
+        stratInputs.append(self.playerStrats_1)
+        stratInputs.append(self.playerStrats_2)
+
+    def onAddPlayerBtnClicked(self):
+        global numPlayers
+        global playersStrats
+
+        numPlayers = numPlayers + 1
+        print("#    Adding players... ", "(Total =", numPlayers, ")")
+        playersStrats = np.append(playersStrats, 2)
+        print("playersStrats = ", playersStrats)
+        self.newLabel = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.newLabel.setObjectName(("label_" + str(numPlayers + 1)))
+        self.newLabel.setText("Player " + str(numPlayers))
+        self.newPlayerStrat = QtWidgets.QLineEdit(self.gridLayoutWidget)
+        self.newPlayerStrat.setObjectName("playerStrats_" + str(numPlayers))
+        self.newPlayerStrat.setText('2')
+        self.formLayout.addRow(self.newLabel, self.newPlayerStrat)
+        stratInputs.append(self.newPlayerStrat)
+
+    def onGenerateCSVBtnClicked(self):
+        global playersStrats
+        global numPlayers
+        global stratInputs
+
+        playersStrats = []
+        for i in range(len(stratInputs)):
+            playersStrats.append(stratInputs[i].text())
+        playersStrats = np.array(playersStrats).astype(int)
+
+        print("#    Generating...")
+        print("numPlayers = ", numPlayers)
+        print("playersStrats = ", playersStrats)
+
+        profiles = []
+        for i in playersStrats:
+            a = np.arange(i)
+            profiles.append(a)
+        result = []
+        for element in itertools.product(*profiles):
+            result.append(element)
+        print(np.array(result))
+        headerList = np.arange(numPlayers)
+        headerList = np.array(headerList).astype(str).tolist()
+        print(headerList)
+        for i in range(len(headerList)):
+            headerList[i] = "Gain " + headerList[i]
+        print(headerList)
+        resultsDataFrame = pd.DataFrame(result)
+        print(resultsDataFrame)
+        resultsDataFrame = resultsDataFrame.reindex(columns=resultsDataFrame.columns.tolist() + headerList)
+        print(resultsDataFrame)
+        resultsDataFrame.to_csv("file.csv", index=None)
+
+    def closeEvent(self, event):
+        global playersStrats
+        global numPlayers
+        global stratInputs
+        numPlayers = 2
+        playersStrats = np.array([2, 2])
+        stratInputs = []
+
+if __name__ == "__main__":
+    # Create the application
+    app = QApplication(sys.argv)
+    # Create and show the application's main window
+    win = Window()
+    win.show()
+    # Run the application's main loop
+    sys.exit(app.exec())
